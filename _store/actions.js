8e6cb3c7bbcacd1f6643f244de027f7167554a92
Yeison Tapia <yftapial@ut.edu.co>
2019-06-17 16:30:02 -0500

    + add format data for q-select

--- _store/actions.js
+++ _store/actions.js
@@ -4,7 +4,7 @@ export const GET_COUNTRIES = ({ commit, state, dispatch }, params = {}) => {
   return new Promise(async (resolve, reject) => {
     await locationsService.crud.index('apiRoutes.qlocations.countries', params)
      .then(response => {
-        commit('SET_COUNTRIES', response.data)
+        commit('SET_COUNTRIES',  response.data.map((item) => { return { label: item.name, value: item.id }}) )
         resolve(true)
      })
      .catch(error=>{
@@ -17,7 +17,7 @@ export const GET_CITIES = ({ commit, state, dispatch }, params = {}) => {
   return new Promise(async (resolve, reject) => {
     await locationsService.crud.index('apiRoutes.qlocations.cities', params)
       .then(response => {
-        commit('SET_CITIES', response.data)
+        commit('SET_CITIES', response.data.map((item) => { return { label: item.name, value: item.id }}) )
         resolve(true)
       })
       .catch(error=>{
@@ -30,7 +30,7 @@ export const GET_PROVINCES = ({ commit, state, dispatch }, params = {}) => {
   return new Promise(async (resolve, reject) => {
     await locationsService.crud.index('apiRoutes.qlocations.provinces', params)
       .then(response => {
-        commit('SET_PROVINCES', response.data)
+        commit('SET_PROVINCES', response.data.map((item) => { return { label: item.name, value: item.id }}) )
         resolve(true)
       })
       .catch(error=>{

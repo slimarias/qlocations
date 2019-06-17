837cbc1191601f79b9581c377a501bcfb2eb5fcd
Yeison Tapia <yftapial@ut.edu.co>
2019-06-17 15:32:00 -0500

    + Add apiRoutes for locations

---
+++ _config/apiRoutes.js
@@ -0,0 +1,10 @@
+const baseApiUrl = `${env('BASE_URL', 'http://localhost:8000')}/api`;
+const moduleName = 'ilocations';
+const moduleVersion = 'v2';
+const urlBase = `${baseApiUrl}/${moduleVersion}/${moduleName}`;
+
+export default {
+  countries : `${urlBase}/countries`,
+  cities : `${urlBase}/cities`,
+  provinces : `${urlBase}/provinces`,
+}

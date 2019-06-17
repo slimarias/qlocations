c15c99b7fd7c57530ed8bb3d9c16d8b00eb8fd98
Yeison Tapia <yftapial@ut.edu.co>
2019-06-17 15:32:30 -0500

    + Add store for locations

---
+++ _store/getters.js
@@ -0,0 +1,14 @@
+// Return shippingMethods
+export function getterShippingMethods (state){
+    return state.shippingMethods
+}
+
+// Return paymentMethods
+export function getterPaymentMethods (state){
+    return state.paymentMethods
+}
+
+// Return paymentMethods
+export function getterPaymentCheckout (state){
+  return state.checkout
+}
